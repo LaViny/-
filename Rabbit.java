f67db6f1b5005faffe2ca2609ca19aef5dfb25e2
LaViny <18851509341@163.com>
2017-02-06 14:53:31 +0800

    Update and rename rabbit.java to Rabbit.java

---
+++ Rabbit.java
@@ -0,0 +1,15 @@
+
+public class Rabbit{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(system.in); 
+  }
+  /**
+    兔子问题：一对兔子出生后.......
+    1,1,2,3,5,8,13,21,35....
+  */
+  
+  private static int rab(int n){
+    if(n){
+    } 
+  }
+}
